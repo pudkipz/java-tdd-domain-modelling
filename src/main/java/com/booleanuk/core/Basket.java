8bf91e12fd1ac2a2f06b8676f54bdc03839febc7
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-09 10:41:30 +0100

    Fix test for adding an already existing item to a basket

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,7 +1,17 @@
 package com.booleanuk.core;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Basket {
+    Map<String, Integer> items = new HashMap<>();
+
     public boolean add(String product, int price) {
+        if (items.containsKey(product)) {
+            return false;
+        }
+
+        items.put(product, price);
         return true;
     }
 }
