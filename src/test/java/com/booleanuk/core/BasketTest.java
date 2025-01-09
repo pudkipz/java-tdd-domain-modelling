54fa9586af4add5edcd5be7cfc7422815a7bab6f
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-01-09 10:54:52 +0100

    Add additional tests

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -35,4 +35,29 @@ public class BasketTest {
         basket.add(product, price);
         Assertions.assertEquals(price, basket.total());
     }
+
+    @Test
+    public void correctCostOfSeveralItems() {
+        Basket basket = new Basket();
+        String prod1 = "eggs";
+        String prod2 = "milk";
+        String prod3 = "flour";
+        int price1 = 20;
+        int price2 = 22;
+        int price3 = 17;
+        basket.add(prod1, price1);
+        basket.add(prod2, price2);
+        basket.add(prod3, price3);
+        Assertions.assertEquals(59, basket.total());
+    }
+
+    @Test
+    public void addingExistingItemShouldNotIncreaseTotal() {
+        Basket basket = new Basket();
+        String product = "eggs";
+        int price = 20;
+        basket.add(product, price);
+        basket.add(product, price);
+        Assertions.assertEquals(20, basket.total());
+    }
 }
